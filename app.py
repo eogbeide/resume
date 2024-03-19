6411be2b542067b7071e4e399b9eac6314edb466
eogbeide <50998708+eogbeide@users.noreply.github.com>
2024-03-18 17:28:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -12,26 +12,18 @@ profile_pic = current_dir / "profile-pic.jpg"
 
 
 # --- GENERAL SETTINGS ---
-PAGE_TITLE = "Digital CV | John Doe"
+PAGE_TITLE = "Digital CV | Manny Ogbeide"
 PAGE_ICON = ":wave:"
-NAME = "John Doe"
+NAME = "Emmanuel Ogbeide"
 DESCRIPTION = """
-Senior Data Analyst, assisting enterprises by supporting data-driven decision-making.
+Senior Technical Product Manager, assisting enterprises by developing, deploying and shipping products.
 """
-EMAIL = "johndoe@email.com"
+EMAIL = "eogbeide@asu.edu"
 SOCIAL_MEDIA = {
     "YouTube": "https://youtube.com/c/codingisfun",
     "LinkedIn": "https://linkedin.com",
     "GitHub": "https://github.com",
     "Twitter": "https://twitter.com",
-}
-PROJECTS = {
-    "🏆 Sales Dashboard - Comparing sales across three stores": "https://youtu.be/Sb0A9i6d320",
-    "🏆 Income and Expense Tracker - Web app with NoSQL database": "https://youtu.be/3egaMfE9388",
-    "🏆 Desktop Application - Excel2CSV converter with user settings & menubar": "https://youtu.be/LzCfNanQ_9c",
-    "🏆 MyToolBelt - Custom MS Excel add-in to combine Python & Excel": "https://pythonandvba.com/mytoolbelt/",
-}
-
 
 st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
 
@@ -73,10 +65,11 @@ st.write('\n')
 st.subheader("Experience & Qulifications")
 st.write(
     """
-- ✔️ 7 Years expereince extracting actionable insights from data
-- ✔️ Strong hands on experience and knowledge in Python and Excel
-- ✔️ Good understanding of statistical principles and their respective applications
-- ✔️ Excellent team-player and displaying strong sense of initiative on tasks
+- ✔️ Mid-level professional
+- ✔️ Strong hands on experience and knowledge in data engineering, business analytics and data story-telling
+- ✔️ Excellent background in supply chain, procurement and finance planning and analytics
+- ✔️ Excellent results-oriented team-player
+- ✔️ Product and program leadership
 """
 )
 
@@ -86,9 +79,9 @@ st.write('\n')
 st.subheader("Hard Skills")
 st.write(
     """
-- 👩‍💻 Programming: Python (Scikit-learn, Pandas), SQL, VBA
-- 📊 Data Visulization: PowerBi, MS Excel, Plotly
-- 📚 Modeling: Logistic regression, linear regression, decition trees
+- 👩‍💻 Programming: Python (Scikit-learn, Pandas), SQL, Alteryx
+- 📊 Data Visulization: Tableau, PowerBi, MS Excel, Plotly
+- 📚 Modeling: Machine Learning (Logistic regression, linear regression, decition trees etcetera)
 - 🗄️ Databases: Postgres, MongoDB, MySQL
 """
 )
@@ -100,44 +93,44 @@ st.subheader("Work History")
 st.write("---")
 
 # --- JOB 1
-st.write("🚧", "**Senior Data Analyst | Ross Industries**")
-st.write("02/2020 - Present")
+st.write("🚧", "**Senior Technical Product Manager | Amazon**")
+st.write("11/2022 - Present")
 st.write(
     """
-- ► Used PowerBI and SQL to redeﬁne and track KPIs surrounding marketing initiatives, and supplied recommendations to boost landing page conversion rate by 38%
-- ► Led a team of 4 analysts to brainstorm potential marketing and sales improvements, and implemented A/B tests to generate 15% more client leads
-- ► Redesigned data model through iterations that improved predictions by 12%
+- ► Lead AI hands-off-the-wheel planning product strategy and delivery across Amazon worldwide businesses
+- ► Lead three-year product strategy and annual planning and product roadmap for Amazon Finance Science team
+- ► Lead Amazon Forecasting Lab initiative for worldwide Finance organization
 """
 )
 
 # --- JOB 2
 st.write('\n')
-st.write("🚧", "**Data Analyst | Liberty Mutual Insurance**")
-st.write("01/2018 - 02/2022")
+st.write("🚧", "**Manager, Finance and Supply Chain/Procurement Planning & Analytics | Amazon Web Services**")
+st.write("06/2019 - 11/2022")
 st.write(
     """
-- ► Built data models and maps to generate meaningful insights from customer data, boosting successful sales eﬀorts by 12%
-- ► Modeled targets likely to renew, and presented analysis to leadership, which led to a YoY revenue increase of $300K
-- ► Compiled, studied, and inferred large amounts of data, modeling information to drive auto policy pricing
+- ► Owned and managed AWS supply chain, finance and procurement end-to-end planning, including data pipeline, and analytics product strategy and delivery.
+- ► Led and oversaw quarterly business planning and review meetings with senior leaders across supply chain and finance organizations.
+- ► Defined, tracked and reported on AWS annual, quarterly and monthly supply chain and finance metrics
 """
 )
 
 # --- JOB 3
 st.write('\n')
-st.write("🚧", "**Data Analyst | Chegg**")
-st.write("04/2015 - 01/2018")
+st.write("🚧", "**Engineer | BHP Billiton Canada**")
+st.write("05/2012 - 03/2015")
 st.write(
     """
-- ► Devised KPIs using SQL across company website in collaboration with cross-functional teams to achieve a 120% jump in organic traﬃc
-- ► Analyzed, documented, and reported user survey results to improve customer communication processes by 18%
-- ► Collaborated with analyst team to oversee end-to-end process surrounding customers' return data
+- ► Led the delivery of three green field exploration and delivery projects for BHP Billiton Jansen multi-billion dollars project.
+- ► Owned capacity planning for Jansen assumption modeling for emerging markets (India, Brazil and China)
+- ► Supported definition studies and operations readiness for new production facilities and delivered the risk and safety management plans and contract management strategy.
 """
 )
 
 
 # --- Projects & Accomplishments ---
-st.write('\n')
-st.subheader("Projects & Accomplishments")
-st.write("---")
-for project, link in PROJECTS.items():
-    st.write(f"[{project}]({link})")
+#st.write('\n')
+#st.subheader("Projects & Accomplishments")
+#st.write("---")
+#for project, link in PROJECTS.items():
+    #st.write(f"[{project}]({link})")
