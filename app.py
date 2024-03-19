2f70c6c1dd17d0131bb39d1e76946c4f6953cc23
eogbeide <50998708+eogbeide@users.noreply.github.com>
2024-03-19 06:35:56 -0700

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ profile_pic = current_dir / "profile-pic.jpg"
 # --- GENERAL SETTINGS ---
 PAGE_TITLE = "Digital CV | Manny Ogbeide"
 PAGE_ICON = ":wave:"
-NAME = "Manny Ogbeide"
+NAME = "Manny Ogbeide, B.Sc.(Hons), M.Sc., MBA"
 DESCRIPTION = """
 - ✔️Snr. Technical Product Manager \n
 - ✔️Helping enterprises to tackle complex, ambiguous and cross-functional business problems & delivering 5-10X value through product and process leadership.
