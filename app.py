a9d4c9740818e39fdc3f96d62119e93c333a582e
eogbeide <50998708+eogbeide@users.noreply.github.com>
2024-03-21 03:34:34 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import streamlit as st
 from PIL import Image
 
+st.set_page_config(layout="wide")
 
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
